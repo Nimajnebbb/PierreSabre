f8fcb7666e27c14d8e40582582f23f322e7d2c0f
Nimajnebbb <114724233+Nimajnebbb@users.noreply.github.com>
2022-11-17 16:27:29 +0100

    classe HistoireTP5

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -29,21 +29,5 @@ public class HistoireTP4 {
 //		roro.direBonjour();
 //		roro.donner(marco);
 //		roro.provoquer(yaku);
-//		TP5
-//		1- Les humains ont une mémoire
-		Commercant marco = new Commercant("Marco", 20);
-		Commercant chonin = new Commercant("Chonin", 40);
-		Commercant kumi = new Commercant("Kumi", 10);
-		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
-		Ronin roro = new Ronin("Roro", "shochu", 60);
-		
-		marco.faireConnaissanceAvec(roro);
-		marco.faireConnaissanceAvec(yaku);
-		marco.faireConnaissanceAvec(chonin);
-		marco.faireConnaissanceAvec(kumi);
-		marco.listerConnaissance();
-		roro.listerConnaissance();
-		yaku.listerConnaissance();
-
 	}
 }
